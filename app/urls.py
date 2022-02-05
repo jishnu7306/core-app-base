51567680569feb9bfeffa5d831f767997894e1dc
jishnu7306 <jishnuksatheeshsj143@gmail.com>
2022-02-05 23:48:20 +0530

    attaching

--- app/urls.py
+++ app/urls.py
@@ -4,9 +4,34 @@ from .import views
 
 urlpatterns = [
     re_path(r'^$', views.index, name='index'),
+
+    ########## Reprt issue
+    #Jishnu
     re_path(r'^reportedissue$', views.reportedissue, name='reportedissue'),
     re_path(r'^reportissue$', views.reportissue, name='reportissue'),
     re_path(r'^reportedissuesub$', views.reportedissuesub, name='reportedissuesub'),
+
+    #saneesha
+    re_path(r'^reportissuetrainers$', views.reportissuetrainers, name='reportissuetrainers'),
+    re_path(r'^reportissuetrainees$', views.reportissuetrainees, name='reportissuetrainees'),
+    re_path(r'^trainerunsolvedissue$', views.trainerunsolvedissue, name='trainerunsolvedissue'),
+    re_path(r'^trainersolvedissue$', views.trainersolvedissue, name='trainersolvedissue'),
+    re_path(r'^traineesunsolved$', views.traineesunsolved, name='traineesunsolved'),
+    re_path(r'^traineessolved$', views.traineessolved, name='traineessolved'),
+
+    
+
+
+
+    ############ Apply leave
+    #jishnu
     re_path(r'^applyleave$', views.applyleave, name='applyleave'),
-    re_path(r'^applyleavesub$', views.applyleavesub, name='applyleavesub')
+    re_path(r'^applyleavesub$', views.applyleavesub, name='applyleavesub'),
+ 
+    #Seban
+    re_path(r'^Requestedleave$', views.Requestedleave, name='Requestedleave'),    
+
+    #Althaf   
+    re_path(r'^trainers_leave$', views.trainers_leave, name='trainers_leave'),
+    re_path(r'^trainees_leave$', views.trainees_leave, name='trainees_leave'),
 ]
--- app/views.py
+++ app/views.py
@@ -2,21 +2,43 @@ from django.http import request
 from django.shortcuts import render
 
 # Create your views here.
+#jishnu
 def index(request):
     return render(request,'index.html')
-
 def reportedissue(request):
     return render(request,'reportedissue.html')
-
 def reportissue(request):
     return render(request,'reportissue.html')
-
 def reportedissuesub(request):
     return render(request,'reportedissuesub.html')
-
 def applyleave(request):
     return render(request,'applyleave.html')
-
 def applyleavesub(request):
     return render(request,'applyleavesub.html')
+
+#saneesha
+def reportissuetrainers(request):
+    return render(request,'reportissuetrainers.html')
+def reportissuetrainees(request):
+    return render(request,'reportissuetrainees.html')
+def trainerunsolvedissue(request):
+    return render(request,'trainerunsolvedissue.html')
+def trainersolvedissue(request):
+    return render(request,'trainersolvedissue.html')
+def traineessolved(request):
+    return render(request,'traineessolved.html')
+def traineesunsolved(request):
+    return render(request,'traineesunsolved.html')
+
+
+
+#sebin
+def Requestedleave(request):
+    return render(request, 'Requestedleave.html')
+
+#althaf
+def trainers_leave(request):
+    return render(request, 'trainers_leave.html')
+def trainees_leave(request):
+    return render(request, 'trainees_leave.html')
 
